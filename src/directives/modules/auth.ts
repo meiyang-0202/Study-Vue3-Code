ab20233dd14da1f30619e0a3ce9103c67a64b7ff
meiyang <413323545@qq.com>
2023-09-28 16:45:04 +0800

    fix: 🧩 九月二十八号推进

--- src/directives/modules/auth.ts
+++ src/directives/modules/auth.ts
@@ -2,7 +2,13 @@
  * v-auth
  * 按钮权限指令
  */
-import { useAuthStore } from "@/stores/modules/auth";
-import type { Directive, DirectiveBinding } from "vue";
+// import { useAuthStore } from "@/stores/modules/auth";
+// import type { Directive, DirectiveBinding } from "vue";
 
-const auth: 
+// const auth: Directive = {
+//   mounted(el: HTMLElement, binding: DirectiveBinding) {
+//     const { value } = binding;
+//   }
+// };
+
+console.log("v-auth");
---
+++ src/stores/modules/auth.ts
@@ -0,0 +1,6 @@
+// import { defineStore } from "pinia";
+// import { AuthState } from "@/stores/interface";
+// import {} from "@/api/modules/login";
+// import {} from "@/utils";
+
+console.log(1);
